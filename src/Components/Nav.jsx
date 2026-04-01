8df5eea140c5aa31f7442a596a65bd5de24506fa
advitya <advitya9@gmail.com>
2026-04-01 16:00:34 +0530

    Initial commit

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,48 @@
+import logo from "../Images/logo.svg"
+import India from "../Images/india-flag.svg"
+import arrow from "../Images/arrow-right-line.svg"
+import Right from "../Components/Right.jsx"
+export default function Nav() {
+  return (
+    <nav className="bg-deepBlue">
+      <div className="relative w-[1080px] mx-auto flex items-center justify-between">
+        <a className="cursor-pointer py-7 pr-7" href="/">
+            <img src={logo} alt="logo" width={125} height={30} />
+        </a>
+        <ul className="flex gap-4">
+            <li className="text-white font-mullish py-7 hover:text-lightBlue cursor-pointer transition-all duration-200 relative group">
+                <a href="#">Payments</a>
+                <div className="absolute bottom-0 w-full h-1 bg-lightBlue hidden group-hover:block transition-all duration-200"></div>
+            </li>
+            <li className="text-white font-mullish py-7 hover:text-lightBlue cursor-pointer transition-all duration-200 relative group">
+                <a href="#">Banking</a>
+                <div className="absolute bottom-0 w-full h-1 bg-lightBlue hidden group-hover:block transition-all duration-200"></div>
+            </li>  
+            <li className="text-white font-mullish py-7 hover:text-lightBlue cursor-pointer transition-all duration-200 relative group">
+                <a href="#">Corporate Card</a>
+            </li>
+            <li className="text-white font-mullish py-7 hover:text-lightBlue cursor-pointer transition-all duration-200 relative group">
+                <a href="#">Payroll</a>
+            </li>  
+            <li className="text-white font-mullish py-7 hover:text-lightBlue cursor-pointer transition-all duration-200 relative group">
+                <a href="#">Resources</a>
+                <div className="absolute bottom-0 w-full h-1 bg-lightBlue hidden group-hover:block transition-all duration-200"></div>
+            </li>
+            <li className="text-white font-mullish py-7 hover:text-lightBlue cursor-pointer transition-all duration-200 relative group">
+                <a href="#">Support</a>
+                <div className="absolute bottom-0 w-full h-1 bg-lightBlue hidden group-hover:block transition-all duration-200"></div>
+            </li> 
+            <li className="text-white font-mullish py-7 hover:text-lightBlue cursor-pointer transition-all duration-200 relative group">
+                <a href="#">Pricing</a>
+                <div className="absolute bottom-0 w-full h-1 bg-lightBlue hidden group-hover:block transition-all duration-200"></div>
+            </li>   
+        </ul>
+        <div className="flex gap-4">
+            <img src={India} alt="India" width={28} height={20}/>
+            <button className="py-3 px-5 font-mullish text-white border border-lightBlue rounded text-sm font-bold">Log in</button>
+            <button className="flex py-3 px-4 font-mullish bg-white text-lightBlue300 font-bold transition-all duration-200 hover:text-lightBlue500 "> Sign Up &nbsp; <Right/> </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
